2a06587f3b29627400f2a024de6cd651e8b708f4
sandhun <pssandhun@gmail.com>
2018-09-24 15:13:06 +0530

    my first commit

---
+++ hello.py
@@ -0,0 +1,11 @@
+#this program says hello and saks for my name
+print('hello world')
+print('what is your name?') #ask for their name
+myname = input()
+print('it is good to meet you' +myname)
+print('the length of your name is :')
+print(len(myname))
+print('what is your age?') #ask for their age
+myage = input()
+print('you will be '  + str(int(myage)+1) +' in a year.')
+
